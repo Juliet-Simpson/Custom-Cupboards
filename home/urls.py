a0b1c7037c9eac3bcfa549777beb1894c70b0cad
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-11-19 00:33:05 +0000

    Created urls, views and templates for remaining pages in the home app.  Templates currently duplicates of index.html. Renamed toast overlay to give a different class from allauth background and alter z-index to put allauth behind footer

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path('', views.index, name='home'),
-] 
+urlpatterns = [	
+	path('', views.index, name='home'),
+	path('', views.about, name='about'),
+	path('', views.contact, name='contact'),
+	path('', views.pricing, name='pricing'),
+	path('', views.delivery, name='delivery'),
+	path('', views.assembly, name='assembly'),
+	path('', views.returns, name='returns'),
+	]
