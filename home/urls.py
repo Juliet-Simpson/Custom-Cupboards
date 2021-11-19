fbdb3a664653abb844ec7d1086aa4ccdf1b3b867
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-11-19 01:53:44 +0000

    Added about page content

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [	
 	path('', views.index, name='home'),
-	path('', views.about, name='about'),
-	path('', views.contact, name='contact'),
-	path('', views.pricing, name='pricing'),
-	path('', views.delivery, name='delivery'),
-	path('', views.assembly, name='assembly'),
-	path('', views.returns, name='returns'),
+	path('about', views.about, name='about'),
+	path('contact', views.contact, name='contact'),
+	path('pricing', views.pricing, name='pricing'),
+	path('deliver', views.delivery, name='delivery'),
+	path('assembly', views.assembly, name='assembly'),
+	path('returns', views.returns, name='returns'),
 	]
